ddcd0974a99d219ab19240cdf9bc095d1828c50f
EmilioC <emicorgul@gmail.com>
2023-02-28 17:53:15 +0100

    Create components

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AgregarComponent,
+    BuscarComponent,
+    HeroeComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
